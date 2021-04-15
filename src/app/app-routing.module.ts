6fe496f4eadc7138b827b9f051879e8c11618cc9
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-04-15 10:11:48 -0500

    diseginh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:LoginComponent},
+  { path: 'dashboard', loadChildren: () => import('./pages/content/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: '**', redirectTo: '' },
+];
+
+
+// ng generate module dashboard --route dashboard --module app.module
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
