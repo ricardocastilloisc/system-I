3d7f8e2e721f1aeca2106e43c57e9474103d3048
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-04-23 09:08:45 -0500

    diseño con tarjetas

--- src/app/shared/content/navbar/navbar.component.ts
+++ src/app/shared/content/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AuthService } from 'src/app/services/auth.service';
 import { AppState } from '../../../ReduxStore/app.reducers';
 import { Usuario } from '../../../model/usuario.model';
 import { Observable } from 'rxjs';
 import { ERole } from '../../../validators/roles';
+
 declare var $: any;
 @Component({
   selector: 'app-navbar',
@@ -12,6 +13,10 @@ declare var $: any;
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.resizeMenuContent();
+  }
   DataUser$: Observable<Usuario>;
 
   Administrador = ERole.Administrador;
@@ -31,21 +36,32 @@ export class NavbarComponent implements OnInit {
     this.authService.signOut();
   };
 
-  rolesValids = (User:Usuario, roles:any[]):boolean => {
+  rolesValids = (User: Usuario, roles: any[]): boolean => {
     return this.authService.rolesValids(User, roles);
-  }
-
+  };
 
   toggle = () => {
     $('#sidebar').toggleClass('active');
+    this.resizeMenuContent();
+  };
 
-    /*
-
-    $("#content").css("background", "red");
-
-    if($("#sidebar").hasClass("active")){
-      console.log('hey hola mundo');
-    }*/
-
+  resizeMenuContent = () => {
+    if (window.innerWidth < 769) {
+      if ($('#sidebar').hasClass('active')) {
+        $('#content').css('margin-left', '253px');
+        $('#headernav').css('width', '85%');
+      } else {
+        $('#content').css('margin-left', '0px');
+        $('#headernav').css('width', '100%');
+      }
+    } else {
+      if ($('#sidebar').hasClass('active')) {
+        $('#content').css('margin-left', '0px');
+        $('#headernav').css('width', '100%');
+      } else {
+        $('#content').css('margin-left', '253px');
+        $('#headernav').css('width', '85%');
+      }
+    }
   };
 }
