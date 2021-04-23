05f48bb9a38d00ae4fb8734bf1a2ebc40820d93b
BGalicia <bgalicia@spsolutions.com.mx>
2021-04-22 19:02:35 -0500

    Agregar servicio para agregar un usuario a un grupo del User Pool

--- src/app/pages/content/dashboard/auditoria/usuarios/usuarios.component.ts
+++ src/app/pages/content/dashboard/auditoria/usuarios/usuarios.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import * as AWS from 'aws-sdk';
+
+var params = {
+  GroupName: "Administrador", /* required */
+  UserPoolId: "us-east-1_Cx1XNNQxU", /* required */
+  Username: "azure_rwayeowx9nsigogmrb6adqmpgrl2hohoivn5bgsobja" /* required */
+};
+
+const SESConfig = {
+  accessKeyId: "AKIAU4J45SEJGVOZU7M6",      // should be:  process.env.AWS_ACCESS_ID
+  secretAccessKey: "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",
+  region: "us-east-1",
+}
+AWS.config.update(SESConfig);
+var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 @Component({
   selector: 'app-usuarios',
@@ -10,6 +25,15 @@ export class UsuariosComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    
+    cognitoidentityserviceprovider.adminAddUserToGroup(params, function (err, data) {
+      console.log("COGNITO IDENTITY SERVICE PROVIDER + USUARIO");
+
+      if (err) console.log(err, err.stack); // an error occurred
+      else console.log(data);           // successful response
+
+    });
   }
 
 }
--- src/app/pages/content/dashboard/home/home.component.ts
+++ src/app/pages/content/dashboard/home/home.component.ts
@@ -3,21 +3,6 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../../../ReduxStore/app.reducers';
 import { Usuario } from 'src/app/model/usuario.model';
 import { Observable } from 'rxjs';
-import * as AWS from 'aws-sdk';
-
-var params = {
-  GroupName: "Ejecutor", /* required */
-  UserPoolId: "us-east-1_Cx1XNNQxU", /* required */
-  Username: "azure_rwayeowx9nsigogmrb6adqmpgrl2hohoivn5bgsobja" /* required */
-};
-
-const SESConfig = {
-  accessKeyId: "AKIAU4J45SEJGVOZU7M6",      // should be:  process.env.AWS_ACCESS_ID
-  secretAccessKey: "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",
-  region: "us-east-1",
-}
-AWS.config.update(SESConfig);
-var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 @Component({
   selector: 'app-home',
@@ -33,13 +18,6 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.DataUser$ = this.store.select(({ usuario }) => usuario.user);
 
-    cognitoidentityserviceprovider.adminAddUserToGroup(params, function (err, data) {
-      console.log("COGNITO IDENTITY SERVICE PROVIDER");
-
-      if (err) console.log(err, err.stack); // an error occurred
-      else console.log(data);           // successful response
-
-    });
   }
 
 }
