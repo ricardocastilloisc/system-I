17006e80401175a7d89408f66f05c86c92b3cc34
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-04-23 10:15:52 -0500

    venta de home con restricciones

--- src/app/pages/content/dashboard/home/home.component.ts
+++ src/app/pages/content/dashboard/home/home.component.ts
@@ -3,21 +3,31 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../../../ReduxStore/app.reducers';
 import { Usuario } from 'src/app/model/usuario.model';
 import { Observable } from 'rxjs';
+import { AuthService } from '../../../../services/auth.service';
+import { ERole } from '../../../../validators/roles';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  DataUser$: Observable<Usuario>;
 
-  DataUser$: Observable<Usuario>
+  Administrador = ERole.Administrador;
+  Ejecutor = ERole.Ejecutor;
+  Soporte = ERole.Soporte;
 
-  constructor(private store: Store<AppState>) { }
+  constructor(
+    private store: Store<AppState>,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.DataUser$ = this.store.select(({ usuario }) => usuario.user);
-
   }
 
+  rolesValids = (User: Usuario, roles: any[]): boolean => {
+    return this.authService.rolesValids(User, roles);
+  };
 }
