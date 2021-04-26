cd810a7820951362f608a2002f210d38d5eca658
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-04-26 10:32:41 -0500

    navbar y diseño de auditoria

---
+++ src/app/helpers/rutas.ts
@@ -0,0 +1,43 @@
+export const rutasConNombres =
+[
+  {
+    rutaAngular: 'auditoria',
+    ValorEsp: 'Auditoría',
+  },
+  {
+    rutaAngular: 'administracion',
+    ValorEsp: 'Administración',
+  },
+  {
+    rutaAngular: 'procesos',
+    ValorEsp: 'Procesos',
+  },
+  {
+    rutaAngular: 'catalogos',
+    ValorEsp: 'Catálogos',
+  },
+  {
+    rutaAngular: 'usuarios',
+    ValorEsp: 'Usuarios',
+  },
+  {
+    rutaAngular: 'diurno',
+    ValorEsp: 'Diurno',
+  },
+  {
+    rutaAngular: 'nocturno',
+    ValorEsp: 'Nocturno',
+  },
+  {
+    rutaAngular: 'diurnos',
+    ValorEsp: 'Diurnos',
+  },
+  {
+    rutaAngular: 'nocturnos',
+    ValorEsp: 'Nocturnos',
+  },
+  {
+    rutaAngular: 'archivos',
+    ValorEsp: 'Achivos',
+  },
+]
