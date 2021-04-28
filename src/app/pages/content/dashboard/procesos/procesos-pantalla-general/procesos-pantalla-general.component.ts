7e5f02dcfae21e1fc86bfe00ff084e7a073fcda0
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-04-28 13:14:35 -0500

    reformatear usuarios

--- src/app/pages/content/dashboard/procesos/procesos-pantalla-general/procesos-pantalla-general.component.ts
+++ src/app/pages/content/dashboard/procesos/procesos-pantalla-general/procesos-pantalla-general.component.ts
@@ -2,14 +2,18 @@ import { LoadAUDGENPROCESOS } from './../../../../../ReduxStore/actions/AUDGENPR
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { APIService, CreateAUDGENPROCESOSInput, ListAUDGENPROCESOSQuery } from '../../../../../API.service';
+import {
+  APIService,
+  CreateAUDGENPROCESOSInput,
+  ListAUDGENPROCESOSQuery,
+} from '../../../../../API.service';
 import { fromPromise } from 'rxjs/observable/fromPromise';
 import { formatDate } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../../../ReduxStore/app.reducers';
 import { Observable } from 'rxjs';
 import { AUDGENPROCESO_INERFACE } from '../../../../../model/AUDGENPROCESO.model';
-
+import { UsuariosService } from '../../../../../services/usuarios.service';
 
 @Component({
   selector: 'app-procesos-pantalla-general',
@@ -17,32 +21,45 @@ import { AUDGENPROCESO_INERFACE } from '../../../../../model/AUDGENPROCESO.model
   styleUrls: ['./procesos-pantalla-general.component.css'],
 })
 export class ProcesosPantallaGeneralComponent implements OnInit {
-
   public createForm: FormGroup;
 
-  inputFecha = formatDate(new Date(),'yyyy-MM-dd', 'en-US');
+  inputFecha = formatDate(new Date(), 'yyyy-MM-dd', 'en-US');
 
+  AUDGENPROCESOS$: Observable<AUDGENPROCESO_INERFACE[]>;
 
-  AUDGENPROCESOS$:Observable<AUDGENPROCESO_INERFACE[]>
+  constructor(
+    private router: Router,
+    private store: Store<AppState>,
+    private fb: FormBuilder,
+    private UsuariosService: UsuariosService,
+    private rutaActiva: ActivatedRoute
+  ) {}
 
-  constructor(private router: Router, private store: Store<AppState>, private fb: FormBuilder, private rutaActiva: ActivatedRoute) { }
-  
   ngOnInit(): void {
     this.createForm = this.fb.group({
-      'ID': ['', Validators.required],
-      'FECHA_FERIADO': ['', Validators.required]
+      ID: ['', Validators.required],
+      FECHA_FERIADO: ['', Validators.required],
     });
 
-    this.AUDGENPROCESOS$ = this.store.select(({AUDGENPROCESOS})=>AUDGENPROCESOS.AUDGENPROCESOS);
+    this.AUDGENPROCESOS$ = this.store.select(
+      ({ AUDGENPROCESOS }) => AUDGENPROCESOS.AUDGENPROCESOS
+    );
 
     this.store.dispatch(LoadAUDGENPROCESOS());
+/*
+    this.UsuariosService.consultarUsuarios().then((res) => {
+      //console.log(this.UsuariosService.reformatearUsuario(res));
+    });
+    */
   }
 
-  botonActivado = (parametocomparar:string):boolean => {
-    return this.rutaActiva.snapshot.params.tipo===parametocomparar?true:false;
-  }
+  botonActivado = (parametocomparar: string): boolean => {
+    return this.rutaActiva.snapshot.params.tipo === parametocomparar
+      ? true
+      : false;
+  };
 
-  consultar(){
-    this.router.navigate(['/'+window.location.pathname+'/proceso']);
+  consultar() {
+    this.router.navigate(['/' + window.location.pathname + '/proceso']);
   }
 }
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -6,24 +6,25 @@ AWS.config.update(environment.SESConfig);
 var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class UsuariosService {
-
   params = {
-    GroupName: "Administrador", /* es un dato de entrada de la pantalla (grupo al que se agrega o remueve el usuario) */
+    GroupName:
+      'Administrador' /* es un dato de entrada de la pantalla (grupo al que se agrega o remueve el usuario) */,
     UserPoolId: environment.UserPoolId,
-    Username: "azure_rwayeowx9nsigogmrb6adqmpgrl2hohoivn5bgsobja" /* identificador del usuario en al user pool */
+    Username:
+      'azure_rwayeowx9nsigogmrb6adqmpgrl2hohoivn5bgsobja' /* identificador del usuario en al user pool */,
   };
 
   paramsUser = {
     UserPoolId: environment.UserPoolId,
-    Username: "azure_rwayeowx9nsigogmrb6adqmpgrl2hohoivn5bgsobja" /* identificador del usuario en al user pool */
+    Username:
+      'azure_rwayeowx9nsigogmrb6adqmpgrl2hohoivn5bgsobja' /* identificador del usuario en al user pool */,
   };
 
   paramsGroups = {
-    UserPoolId: environment.UserPoolId
+    UserPoolId: environment.UserPoolId,
   };
 
   paramsUsers = {
@@ -34,50 +35,94 @@ export class UsuariosService {
     ],
     Filter: 'STRING_VALUE',
     Limit: 'NUMBER_VALUE',
-    PaginationToken: 'STRING_VALUE'
+    PaginationToken: 'STRING_VALUE',
   };
 
   paramsUserGroups = {
-    GroupName: "Administrador", /* es un dato de entrada de la pantalla */
+    GroupName: 'Administrador' /* es un dato de entrada de la pantalla */,
     Limit: environment.Limit,
     UserPoolId: environment.UserPoolId,
   };
 
-  constructor() { }
+  constructor() {}
 
   consultarGrupos(): void {
     // metodo para consultar todos los grupos del user pool
-    cognitoidentityserviceprovider.listGroups(this.paramsGroups, this.callbackAws);
+    cognitoidentityserviceprovider.listGroups(
+      this.paramsGroups,
+      this.callbackAws
+    );
   }
 
-  consultarUsuarios(): void {
+  consultarUsuarios() {
     // metodo para consultar todos los usuarios del user pool
-    cognitoidentityserviceprovider.listUsers(this.paramsGroups, this.callbackAws);
+    return cognitoidentityserviceprovider
+      .listUsers(this.paramsGroups)
+      .promise();
   }
 
   consultarUsuariosEnGrupo(): void {
     // metodo para consultar todos los usuarios que pertenecen a un grupo dentro del user pool
-    cognitoidentityserviceprovider.listUsersInGroup(this.paramsUserGroups, this.callbackAws);
+    cognitoidentityserviceprovider.listUsersInGroup(
+      this.paramsUserGroups,
+      this.callbackAws
+    );
   }
 
   obtenerDetalleUsuario(): void {
     // metodo para obtener el los datos a detalle del usuario
-    cognitoidentityserviceprovider.adminGetUser(this.paramsUser, this.callbackAws);
+    cognitoidentityserviceprovider.adminGetUser(
+      this.paramsUser,
+      this.callbackAws
+    );
   }
 
   agregarUsuarioGrupo(): void {
     // metodo para agregar a un usuario habilitado en el user pool a un grupo en especifico
-    cognitoidentityserviceprovider.adminAddUserToGroup(this.params, this.callbackAws);
+    cognitoidentityserviceprovider.adminAddUserToGroup(
+      this.params,
+      this.callbackAws
+    );
   }
 
   eliminarUsuarioGrupo(): void {
     // metodo para remover a un usuario habilitado en el user pool a un grupo en especifico al que pertenece
-    cognitoidentityserviceprovider.adminRemoveUserFromGroup(this.params, this.callbackAws);
+    cognitoidentityserviceprovider.adminRemoveUserFromGroup(
+      this.params,
+      this.callbackAws
+    );
   }
 
   callbackAws = (err, data) => {
     if (err) console.log(err, err.stack);
     else console.log(data);
-  }
+  };
+  /*
+ayuda de atibutos: {Name: "sub", Value: "42ae1b55-8029-4a09-8c81-8c805c650aaf"}
+1: {Name: "identities", Value: "[{"userId":"PY5dp6qYCyodowdB_EBAmPy3aF9cV6iO1-k6Ue…null,"primary":true,"dateCreated":1618943540138}]"}
+2: {Name: "email_verified", Value: "false"}
+3: {Name: "given_name", Value: "Diego"}
+4: {Name: "family_name", Value: "Garcia"}
+5: {Name: "email", Value: "garcia.diego@principal.com"}
+*/
 
+  reformatearArrayDeUsuarios = (objectUsers) => {
+    let arrayUsers = [];
+    console.log(objectUsers)
+    objectUsers.Users.forEach(objectUser =>{
+      let object = {
+        UserCreateDate: objectUser.UserCreateDate,
+        UserLastModifiedDate: objectUser.UserLastModifiedDate,
+        Enabled: objectUser.Enabled,
+        UserStatus: objectUser.UserStatus,
+        Username: objectUser.Username,
+        Attributes: {},
+      };
+      objectUser.Attributes.forEach( attribute => {
+        object.Attributes[attribute.Name] = attribute.Value;
+      });
+      arrayUsers.push(object)
+    });
+    return arrayUsers
+  };
 }
