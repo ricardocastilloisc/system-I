d36ab721541deb5754d36df63a8d1cec9e8afa9a
BGalicia <bgalicia@spsolutions.com.mx>
2021-05-04 07:24:49 -0500

    actualizacion roles

--- src/app/validators/roles.ts
+++ src/app/validators/roles.ts
@@ -1,15 +1,20 @@
+// callbackAwsDetalle --> var rol = data['UserAttributes'].find((el) => el.Name == 'custom:rol')['Value'];
 export enum ERole {
   Administrador = 'Administrador',
   Ejecutor = 'Ejecutor',
-  Soporte = 'Soporte',
-  AdministradorArea = 'AdministradorArea'
+  Soporte = 'Soporte'
 }
-
-
-export enum AREAS {
+// callbackAwsDetalle --> var negocio = data['UserAttributes'].find((el) => el.Name == 'custom:negocio')['Value'];
+export enum ENegocio {
   Afore = 'Afore',
   Fondos = 'Fondos',
   Seguros = 'Seguros',
-  Afore_Fondos = 'Afore/Fondos',
-  Tesoreria = 'Tesorería'
+  Afore_Fondos = 'Afore y Fondos'
+}
+// DataUser --> user.usuario.groups
+export enum AREA {
+  Tesoreria = 'Tesoreria',
+  Inversiones_Riesgos = 'Inversiones y Riesgos',
+  Contabilidad = 'Contabilidad',
+  Custodia = 'Custodia'
 }
