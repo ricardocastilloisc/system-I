afd174b2548b8fd20a9b41f810fae9b2f51a2ee7
Victor Hernandez <vhernandez@spsolutions.com.mx>
2021-05-28 17:44:24 -0500

    Fix neva estructura etapas  proceso

--- src/app/model/AUDGENESTADOPROCESO.model.ts
+++ src/app/model/AUDGENESTADOPROCESO.model.ts
@@ -2,23 +2,30 @@ export interface AUDGENESTADOPROCESO_INTERFACE {
     __typename: "SiaGenAudEstadoProcesosDev";
     ESTADO?: string | null;
     ESTADO_EJECUCION?: string | null;
-    ETAPA?: {
-      __typename: "siaGenAudEtapas";
-      INICIAL?:{
-        __typename: "ETAPA";
-        ESTADO?: string | null;
-      };
-      PROCESAMIENTO?: {
-        __typename: "ETAPA";
-        ESTADO?: string | null;
-      };
-      FINAL?:{
-        __typename: "ETAPA";
-        ESTADO?: string | null;
-      };
-    };
+    ETAPA_INICIAL?: {
+      __typename: "ETAPAS_PROCESO";
+      ESTADO_INICIAL?: string | null;
+      ESTADO_FINAL?: string | null;
+      FECHA_INICIAL?: string | null;
+      FECHA_FINAL?: string | null;
+    } | null;
+    ETAPA_FINAL?: {
+      __typename: "ETAPAS_PROCESO";
+      ESTADO_INICIAL?: string | null;
+      ESTADO_FINAL?: string | null;
+      FECHA_INICIAL?: string | null;
+      FECHA_FINAL?: string | null;
+    } | null;
+    ETAPA_PROCESAMIENTO?: {
+      __typename: "ETAPAS_PROCESO";
+      ESTADO_INICIAL?: string | null;
+      ESTADO_FINAL?: string | null;
+      FECHA_INICIAL?: string | null;
+      FECHA_FINAL?: string | null;
+    } | null;
     FECHA_ACTUALIZACION?: string | null;
     FECHA_CREADO?: string | null;
+    FECHA_FINALIZADO?: string | null;
     ID_PROCESO?: string | null;
     INSUMO?: string | null;
     INTERFAZ?: string | null;
