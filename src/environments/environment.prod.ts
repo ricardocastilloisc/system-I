e99c589c32e8d7c14db9aaf7989c3133c4b4814c
BGalicia <bgalicia@spsolutions.com.mx>
2021-06-17 15:37:25 -0500

    :wrench: Actualizar endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,57 +1,74 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
+  production: true,
 
-
-  SESConfig:{
-    accessKeyId: "AKIAU4J45SEJGVOZU7M6",      // should be:  process.env.AWS_ACCESS_ID
-    secretAccessKey: "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",
-    region: "us-east-1",
+  SESConfig: {
+    accessKeyId: 'AKIAU4J45SEJH3NKEMHX',
+    secretAccessKey: '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',
+    region: 'us-east-1',
   },
-  
-  /*urlExternalLogin:
-    'https://auth-335672086802-us-east-1.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=jll4ul3pmr6rb37i0fmtmctf&redirect_uri=https://dev.d23mbxjbgl0msz.amplifyapp.com/',
 
-  urlExternalLogin:
-    'https://auth-335672086802-us-east-1.auth.us-east-1.amazoncognito.com/oauth2/authorize?redirect_uri=http://localhost:4200/dashboard&response_type=token&client_id=jll4ul3pmr6rb37i0fmtmctf&identity_provider=Okta&scope=phone%20email%20openid%20profile&state=aYnW5M8HLLjqDNFngU8Zut5Y5piMOrwC&code_challenge=nfmOGFZT1-rvebusaLLXJzVRvLPdChqT8OahbWfcRK&code_challenge_method=S256',
-    */
-    urlExternalLogin:
-    'https://sia-app.auth.us-east-1.amazoncognito.com/oauth2/authorize?identity_provider=Azure&redirect_uri=http://localhost:4200/dashboard&response_type=token&client_id=24jfvl8v3gb5peslfvaf036m5l&state=usSu0K88JAUtvsDi9ldADWEezLdvFzqQ&code_challenge=GpAjcPWwYZ7Xrge2-dS2BdA7C4UNJcOfNzQVpdp2tN8&code_challenge_method=S256&scope=phone%20email%20openid%20profile',
+  // urlExternalLogin: 'https://sia-app-up.auth.us-east-1.amazoncognito.com/oauth2/authorize?identity_provider=Azure&redirect_uri=https://dev.d23mbxjbgl0msz.amplifyapp.com/&response_type=code&client_id=5cvq4jop6octili7583n597kd1&scope=phone%20email%20openid%20profile',
+
+  urlExternalLogin: 'https://sia-app-up.auth.us-east-1.amazoncognito.com/oauth2/authorize?identity_provider=Azure&redirect_uri=http://localhost:4200/dashboard&response_type=code&client_id=5cvq4jop6octili7583n597kd1&scope=phone%20email%20openid%20profile',
+
   amplifyConfig: {
     Auth: {
-      region: "us-east-1",
-      userPoolId: "us-east-1_Cx1XNNQxU",
-      userPoolWebClientId: "24jfvl8v3gb5peslfvaf036m5l",
+      region: 'us-east-1',
+      userPoolId: 'us-east-1_Ftm7m3Om2',
+      userPoolWebClientId: '5cvq4jop6octili7583n597kd1',
+      limit: 50,
       oauth: {
-        domain: "sia-app.auth.us-east-1.amazoncognito.com",
-        scope: ["phone", "email", "openid", "profile"],
-        redirectSignIn: "localhost:4200/dashboard",
-        redirectSignOut: "localhost:4200",
-        responseType: "token"
+        domain: 'sia-app-up.auth.us-east-1.amazoncognito.com',
+        scope: ['phone', 'email', 'openid', 'profile'],
+/*
+        redirectSignIn: 'https://dev.d23mbxjbgl0msz.amplifyapp.com/',
+        redirectSignOut: 'https://dev.d23mbxjbgl0msz.amplifyapp.com/',
+*/
+        redirectSignIn: 'http://localhost:4200/dashboard',
+        redirectSignOut: 'http://localhost:4200',
+
+        responseType: 'code'
       },
-      /*region: "us-east-1",
-      userPoolId: "us-east-1_mRG6EmrII",
-      userPoolWebClientId: "jll4ul3pmr6rb37i0fmtmctf",
-      oauth: {
-        domain: "auth-335672086802-us-east-1.auth.us-east-1.amazoncognito.com",
-        scope: ["phone", "email", "openid", "profile"],
-        redirectSignIn: "localhost:4200/dashboard",
-        redirectSignOut: "localhost:4200",
-        responseType: "token"
-      },*/
-    },
-    API: {
-      endpoints: [
-        {
-          name: 'main',
-          endpoint:
-            'https://tnjo8cjoj1.execute-api.us-east-1.amazonaws.com/prod/', // for local test change to something such as 'http://localhost:3001'
-        },
-      ],
-    },
+    }
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'sqs-auditoria',
+        endpoint: 'https://sqs.us-east-1.amazonaws.com/335672086802/sia-utileria-encolamiento-mensajes-monitoreo-dev.fifo'
+      },
+      {
+        name: 'catalogos',
+        endpoint: 'https://ixsp0lvu2h.execute-api.us-east-1.amazonaws.com/dev/sia/utileria/'
+      },
+      {
+        name: 'auditoria',
+        endpoint: 'https://ixsp0lvu2h.execute-api.us-east-1.amazonaws.com/dev/sia/utileria/auditoria/interfaces'
+      },
+      {
+        name: 'AIMS Y EXCEDENTES',
+        endpoint: 'https://ixsp0lvu2h.execute-api.us-east-1.amazonaws.com/dev/sia/afore/aimsexcedentes'
+      },
+      {
+        name: 'MD',
+        endpoint: 'https://ixsp0lvu2h.execute-api.us-east-1.amazonaws.com/dev/sia/afore/md'
+      },
+      {
+        name: 'INT CASH',
+        endpoint: 'https://ixsp0lvu2h.execute-api.us-east-1.amazonaws.com/dev/sia/afore/intcash'
+      },
+      {
+        name: 'MO',
+        endpoint: 'https://ixsp0lvu2h.execute-api.us-east-1.amazonaws.com/dev/sia/afore/mo'
+      },
+      {
+        name: 'CRD',
+        endpoint: 'https://ixsp0lvu2h.execute-api.us-east-1.amazonaws.com/dev/sia/fondos/crd'
+      },
+      {
+        name: 'MANDATOS',
+        endpoint: 'https://ixsp0lvu2h.execute-api.us-east-1.amazonaws.com/dev/sia/fondos/mandatos'
+      }
+    ],
   },
 };
-
